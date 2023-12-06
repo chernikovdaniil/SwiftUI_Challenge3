cf36279f52290fc5ecd24774a1b94222ed962fe0
Danil Chernikov <danil.chernikov@onix-systems.com>
2023-12-06 22:52:40 +0100

    Initial Commit

---
+++ SwiftUI_Challenge3/SwiftUI_Challenge3App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Challenge3App.swift
+//  SwiftUI_Challenge3
+//
+//  Created by Danil Chernikov on 06.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Challenge3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
