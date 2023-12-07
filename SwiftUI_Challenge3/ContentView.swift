a6464122b39e4182c995a3ada4ae0ab5d8f29915
Danil Chernikov <danil.chernikov@onix-systems.com>
2023-12-07 03:44:40 +0100

    Tried to do that hard animation.

--- SwiftUI_Challenge3/ContentView.swift
+++ SwiftUI_Challenge3/ContentView.swift
@@ -7,18 +7,54 @@
 
 import SwiftUI
 
+#Preview {
+    ContentView()
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            PlayButton()
         }
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct PlayButton: View {
+    @State fileprivate var isPlay = false
+    
+    private let iconSize: CGSize = .init(width: 40, height: 50)
+    
+    var body: some View {
+        Button {
+            isPlay = false
+            withAnimation(.spring(response: 0.7,
+                                  dampingFraction: 0.5,
+                                  blendDuration: .zero)) {
+                isPlay.toggle()
+            }
+        } label: {
+            HStack(spacing: .zero) {
+                playIcon
+                    .frame(width: isPlay ? iconSize.width : .zero)
+                    .opacity(isPlay ? 1 : .zero)
+                
+                playIcon
+                
+                withAnimation(.interpolatingSpring(stiffness: 50, damping: 10)) {
+                    playIcon
+                        .frame(width: isPlay ? .zero : iconSize.width)
+                        .scaleEffect(isPlay ? .zero : 1)
+                        .opacity(isPlay ? .zero : 1)
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    var playIcon: some View {
+        Image(systemName: "play.fill")
+            .resizable()
+            .frame(width: iconSize.width, height: iconSize.height)
+    }
 }
